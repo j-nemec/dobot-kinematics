e77bc6aba4c9cc5db79191a686765afe2a192d0d
j-nemec <josef.nemec.m@icloud.com>
2025-12-10 14:18:15 +0100

    Oprava rozsahu config.h

--- include/config.h
+++ include/config.h
@@ -19,25 +19,17 @@
 #define L1 135.0                    /* mm */
 #define L2 147.0                    /* mm */
 
-/* Pracovní prostor specifikovaný dokumentací robota */
-#define WORKSPACE_X_MIN_MM  0.0     /* mm */
-#define WORKSPACE_X_MAX_MM  250.0   /* mm */
-#define WORKSPACE_Y_MIN_MM -125.0   /* mm */
-#define WORKSPACE_Y_MAX_MM  125.0   /* mm */
-#define WORKSPACE_Z_MIN_MM  0.0     /* mm */
-#define WORKSPACE_Z_MAX_MM  250.0   /* mm */
-
 /* Výška osy J1 nad podložkou (Z) */
 #define BASE_Z 138.0  /* mm */
 /* J1 - rotace základny 180° */
-#define J1_MIN_DEG   (-90.0)
-#define J1_MAX_DEG   (+90.0)
+#define J1_MIN_DEG   (-125.0)
+#define J1_MAX_DEG   (+125.0)
 /* J2 - rameno L1 */
 #define J2_MIN_DEG     (-5.0)
-#define J2_MAX_DEG    (+85.0)
+#define J2_MAX_DEG    (+90.0)
 /* J3 - rameno L2 */
-#define J3_MIN_DEG   (-10.0)
-#define J3_MAX_DEG   (90.0)
+#define J3_MIN_DEG   (-15.0)
+#define J3_MAX_DEG   (68.0)
 /* J4 - rotace nástroje */
 #define J4_MIN_DEG   (-90.0)
 #define J4_MAX_DEG   (+90.0)
@@ -46,7 +38,13 @@
 #define EPS_ANG_RAD  (1e-6) // Úhel cca 0.000057°
 
 /* Experimentální test */
-//#define REACH_MIN_MM ( (L1)>(L2) ? (L1)-(L2) : (L2)-(L1) )
-//#define REACH_MAX_MM ( (L1)+(L2) )
+
+#define REACH_MAX_MM ( (L1)+(L2) )
+
+/* Pracovní prostor specifikovaný dokumentací robota 2D */
+#define WORKSPACE_X_MIN_MM  10.0     /* mm */
+#define WORKSPACE_X_MAX_MM  328.5   /* mm */
+#define WORKSPACE_Z_MIN_MM  -146.4     /* mm */
+#define WORKSPACE_Z_MAX_MM  172.5   /* mm */
 
 #endif /* CONFIG_H */
